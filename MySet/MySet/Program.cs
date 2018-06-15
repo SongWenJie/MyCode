26e87cba3fa7926b48c4e0c8d42244ecfc07fb84
SongWenJie <719172036@qq.com>
2018-06-15 10:55:54 +0800

    自定义集合实现（迭代器模式）

---
+++ MySet/MySet/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] values = { "a", "b", "c", "d", "e" };
+            MySet mySet = new MySet(values);
+            foreach (var item in mySet)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+
+    public class MySet : IEnumerable
+    {
+        internal object[] values;
+
+        public MySet(object[] values)
+        {
+            this.values = values;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                yield return values[i];
+            }
+        }
+    }
+
+    public class MySetIterator : IEnumerator
+    {
+        MySet set;
+        /// <summary>
+        /// 保存迭代到的位置
+        /// </summary>
+        int position;
+        internal MySetIterator(MySet set)
+        {
+            this.set = set;
+            position = -1;
+        }
+
+        public object Current
+        {
+            get
+            {
+                if(position==-1||position==set.values.Length)
+                {
+                    throw new InvalidOperationException();
+                }
+                int index = position;
+                return set.values[index];
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if(position!=set.values.Length)
+            {
+                position++;
+            }
+            return position < set.values.Length;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+    }
+
+}
