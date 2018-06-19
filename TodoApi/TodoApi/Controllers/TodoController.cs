2170247b152f47410a4dd1040c84e70aea7b79cd
SongWenJie <719172036@qq.com>
2018-06-19 16:50:23 +0800

    core webapi

---
+++ TodoApi/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        public ITodoRepository _todoRepository;
+        public TodoController(ITodoRepository todoRepository)
+        {
+            _todoRepository = todoRepository;
+        }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return _todoRepository.GetAll();
+        }
+
+        [HttpGet("{id}",Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = _todoRepository.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]TodoItem item)
+        {
+            if(item==null)
+            {
+                return BadRequest();
+            }
+            _todoRepository.Add(item);
+
+            return CreatedAtRoute("GetTodo", new { controller = "Todo", id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id,[FromBody]TodoItem item)
+        {
+            if(item == null || id != item.Key)
+            {
+                return BadRequest();
+            }
+
+            var todo = _todoRepository.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _todoRepository.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            _todoRepository.Remove(id);
+        }
+    }
+}
