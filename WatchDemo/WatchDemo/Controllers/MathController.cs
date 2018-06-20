8d4b6b44fea08ac54c2a78fd97f0b7aebd24b971
SongWenJie <719172036@qq.com>
2018-06-20 17:06:00 +0800

    watcher 修改代码  自动重启项目

---
+++ WatchDemo/WatchDemo/Controllers/MathController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WatchDemo.Controllers
+{
+    /// <summary>
+    /// Nuget安装Watcher修改代码 监控 自动重启服务
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    public class MathController : Controller
+    {
+        public int Sum(int a,int b)
+        {
+            return a + b;
+        }
+
+        public int Product(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
